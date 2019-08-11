80dc9092e03563ad22697ce951f49fc946d7b640
Arnab Mondal <arnab2807@gmail.com>
2019-08-11 23:33:28 +0530

    Implemented security, not working

--- src/main/java/com/lms/svc/common/model/AuthenticatedUser.java
+++ src/main/java/com/lms/svc/common/model/AuthenticatedUser.java
@@ -1,5 +1,7 @@
 package com.lms.svc.common.model;
 
+import java.util.List;
+
 import com.lms.svc.common.constants.ApplicationCommonConstants;
 
 import lombok.Data;
@@ -7,12 +9,10 @@ import lombok.Data;
 @Data
 public class AuthenticatedUser {
 	private String userName;
-	private String userRight;
+	private String fullName;
+	private List<String> userRights;
 	private String userStatus;
 	
-	public boolean isAdmin() {
-		return ApplicationCommonConstants.USER_RIGHT_ADMIN.equalsIgnoreCase(userRight);
-	}
 	public boolean isActive() {
 		return ApplicationCommonConstants.USER_STATUS_CODE_ACTIVE.equalsIgnoreCase(userStatus);
 	}
